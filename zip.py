25926e29a65f4c1dfee0cd9676fcbb2474a98432
linuxjustin <30632275+linuxjustin@users.noreply.github.com>
2017-08-12 18:19:24 +0530

    Add files via upload

---
+++ zip.py
@@ -0,0 +1,45 @@
+###@@@###Zip Cracker v2 ###@@###
+justin = '''
++=======================================+
+|..........Zip Cracker v 2.............................................|
++--------------------------------------------------------------+
+|#Author: JUSTIN |
+|#Contact: https://www.facebook.com/
+groups/655647931192526 / |
+|#Date: 11/08/2017 |
+|#this tools just for test |
+|#i dont' need the opportunity to hack |
++=======================================+
+|..........Zip Cracker...........................................|
++-------------------------------------------------------------+
+'''
+print " Usage: " + "-f <zip file> -d <dict> "
+print justin
+import zipfile
+import optparse
+from threading import Thread
+def a(z,password):
+    try:
+        z.extractall(pwd=password)
+        print "!!PASSWORD= " + password + '\n'
+    except:
+        pass
+    def main():
+        parse = optparse.OptionParser("Usage: " + "-f <zip file> -d <dict>")
+        parse.add_option("-f",dest="jus",type="string",help="zip file")
+        parse.add_option("-d",dest="tin",type="string",help="password file")
+        (options,args) = parse.parse_args()
+        if (options.jus == None) | (options.tin == None):
+            print parse.usage
+            exit(0)
+        else:
+            jus = options.jus
+            tin = options.tin
+            z=zipfile.ZipFile(jus)
+            pq=open(tin)
+            for x in pq.readlines():
+                password = x.strip('\n')
+                t = Thread(target=a,args=(z,password))
+                t.start()
+                if __name__ == '__main__':
+                    main()
